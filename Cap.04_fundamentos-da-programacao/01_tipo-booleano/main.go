db5164ed85fbfcb33e41d07cdd4e43af1fb81fa3
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-07-29 13:19:31 -0300

    -Tipo booleano

---
+++ Cap.04_fundamentos-da-programacao/01_tipo-booleano/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var x bool
+
+func main() {
+	fmt.Println(x) // zero value
+	x = true
+	fmt.Println(x) // valor atribuído
+	x = 10 < 100
+	y := 10 == 100
+	z := 10 > 100
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
