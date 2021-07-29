6ff70b70ba5cace5de7273a899d7508a17a51aa0
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-07-29 18:03:10 -0300

    -Tipo string (cadeias de caracteres)

---
+++ Cap.04_fundamentos-da-programacao/05_tipo-string/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "ascii éøâ 香"
+
+	for _, v := range s {
+		fmt.Printf("%b - %T - %#U - %#x\n", v, v, v, v)
+	}
+
+	fmt.Println("")
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%b - %T - %#U - %#x\n", s[i], s[i], s[i], s[i])
+	}
+}
