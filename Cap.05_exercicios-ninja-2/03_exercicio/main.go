a672d5bae39a55fcfd54f55ef2a9611742a33817
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-07-29 22:12:35 -0300

    -Exercicio 03

---
+++ Cap.05_exercicios-ninja-2/03_exercicio/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+/*
+- Crie constantes tipadas e não-tipadas.
+- Demonstre seus valores.
+*/
+
+const x int = 10
+const y = 10
+
+func main() {
+	fmt.Println(x, y)
+}
