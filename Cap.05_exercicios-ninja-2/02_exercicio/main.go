cc8eefa0feedf0b9862f0487de4bd8eedaffa2d5
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-07-29 22:09:17 -0300

    -Exercicio 02

---
+++ Cap.05_exercicios-ninja-2/02_exercicio/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+/*
+- Escreva expressões utilizando os operadores, e atribua seus valores a variáveis.
+- Demonstre estes valores.
+*/
+
+func main() {
+	a := (10 == 100)
+	b := (10 != 100)
+	c := (10 <= 100)
+	d := (10 < 100)
+	e := (10 >= 100)
+	f := (10 > 100)
+	fmt.Printf("%v\n%v\n%v\n%v\n%v\n%v\n", a, b, c, d, e, f)
+}
