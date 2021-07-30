d399ff4fb655b7d63506648574fe5289e1c8da49
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-07-29 22:21:38 -0300

    -Exercicio 06

---
+++ Cap.05_exercicios-ninja-2/06_exercicio/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+/*
+- Utilizando iota, crie 4 constantes cujos valores sejam os próximos 4 anos.
+- Demonstre estes valores.
+*/
+
+const (
+	_ = 1994 + iota
+	b
+	c
+	d
+	e
+)
+
+func main() {
+	fmt.Println(b, c, d, e)
+}
