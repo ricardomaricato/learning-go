e0c15355bbafff8738e37e2240afda77f355dc7c
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-03 18:21:56 -0300

    -Exercícios: Nível #4 – 7

---
+++ Cap.09_exercicios-ninja-4/07_exercicio/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+/*
+- Crie uma slice contendo slices de strings ([][]string). Atribua valores a este slice multi-dimensional da seguinte maneira:
+    - "Nome", "Sobrenome", "Hobby favorito"
+- Inclua dados para 3 pessoas, e utilize range para demonstrar estes dados.
+*/
+
+func main() {
+	ss := [][]string{
+		[]string{
+			"miu",
+			"milton",
+			"encher o saco",
+		},
+		[]string{
+			"mimi",
+			"martha",
+			"pedir comida",
+		},
+		[]string{
+			"meus alunos queridos",
+			"que estudam bastante",
+			"fazer os exercícios ninja",
+		},
+	}
+
+	for _, v := range ss {
+		fmt.Println(v)
+	}
+
+	fmt.Println("\n\n")
+
+	for _, v := range ss {
+		fmt.Println(v[0])
+		for _, item := range v {
+			fmt.Println("\t", item)
+		}
+
+	}
+
+}
