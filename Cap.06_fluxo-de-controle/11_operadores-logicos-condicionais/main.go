1617ef31b66c6189241490bb88df624300dbb70c
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-03 14:04:13 -0300

    -Operadores lógicos condicionais

---
+++ Cap.06_fluxo-de-controle/11_operadores-logicos-condicionais/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+/*
+- &&
+- ||
+- !
+- Go Playground: https://play.golang.org/p/MFwrt93xlc
+- Qual o resultado de fmt.Println...
+    - true && true
+    - true && false
+    - true || true
+    - true || false
+    - !true
+*/
+
+func main() {
+	x := 9
+
+	if !(x%2 == 0) && x%3 == 0 {
+		fmt.Println("é múltiplo de dois e tambem de treis")
+	}
+
+	//	if x%2 == 0 || x%3 == 0 {
+	//		fmt.Println("é múltiplo de dois ou de treis")
+	//	}
+}
