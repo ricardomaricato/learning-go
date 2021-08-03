54aabe3837220486d0c1e0d5d1194db97f01cddc
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-03 14:22:50 -0300

    -Exercícios: Nível #3 – 6

---
+++ Cap.07_exercicios-ninja-3/06-exercicio/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+/*
+- Crie um programa que demonstre o funcionamento da declaração if.
+*/
+
+func main() {
+	tômortadecansaço := true
+	if tômortadecansaço {
+		fmt.Println("um dia eu gostaria de ir pra cama")
+	}
+}
