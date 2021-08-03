f322a6ac79e379bb5a20044d015a5667cefd6390
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-03 14:35:41 -0300

    -Agrupamentos de Dados – 2. Slice: literal composta

---
+++ Cap.08_agrupamento-de-dados/02_slice_literal-composta/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+/*
+- O que são tipos de dados compostos?
+    - Wikipedia: Composite_data_type
+    - Effective Go: Composite literals
+    - ref/spec: Composite literals
+- Uma slice agrupa valores de um único tipo.
+- Criando uma slice: literal composta → x := []type{values}
+*/
+
+func main() {
+	array := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(array)
+	slice := []int{1, 2, 3, 4, 5}
+	fmt.Println(slice)
+
+	slice2 := append(slice, 6)
+	fmt.Println(slice2)
+
+	fmt.Println(slice[3])
+	slice[3] = 348756
+	fmt.Println(slice[3])
+	slice[20] = 1
+	fmt.Println(slice[20])
+}
