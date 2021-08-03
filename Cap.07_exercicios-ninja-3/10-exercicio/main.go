51c8973de238e14bd9178c3b82c1efad74dc9194
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-03 14:27:58 -0300

    -Exercícios: Nível #3 – 10

---
+++ Cap.07_exercicios-ninja-3/10-exercicio/main.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+- Anote (à mão) o resultado das expressões:
+    - fmt.Println(true && true) => true
+    - fmt.Println(true && false) => false
+    - fmt.Println(true || true) => true
+    - fmt.Println(true || false) => true
+    - fmt.Println(!true) => false
+- Ninja nível 3! Parabéns!
+*/
+
+func main() {
+
+}
