e2685df0853a0a87a72cb2476f2484ae7e878171
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-04 16:11:29 -0300

    -Funções – 7. Func como expressão

---
+++ Cap.12_funcoes/07_func-como-expressao/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+/*
+- f := func(p params){ ... }
+- f()
+*/
+
+func main() {
+
+	x := 12
+
+	y := func(x int) int {
+		//fmt.Println(x, "vezes 873648 é:")
+		return x * 873648
+	}
+
+	fmt.Println(x, "vezes 873648 é:", y(x))
+
+}
