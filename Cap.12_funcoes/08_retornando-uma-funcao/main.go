443a4551f4bd9a38f6064d98ebaac23653cc9b65
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-04 17:08:04 -0300

    -Funções – 8. Retornando uma função

---
+++ Cap.12_funcoes/08_retornando-uma-funcao/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+/*
+- Pode-se usar uma função como retorno de uma função
+- Declaração: func f() return
+- Exemplo: func f() func() int { [...]; return func() int{ return [int] } }
+    - ????: fmt.Println(f()())
+*/
+
+func main() {
+
+	x := retornaumafuncao()
+
+	y := x(3)
+
+	fmt.Println(y)
+
+	fmt.Println(retornaumafuncao()(3))
+
+}
+
+func retornaumafuncao() func(int) int {
+	return func(i int) int {
+		return i * 10
+	}
+}
