b73c200030bfa82a457cc14b292373c60c214186
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-05 14:41:28 -0300

    -Aplicações – 1. Documentação JSON

---
+++ Cap.16_aplicacoes/01_documentacao-json/main.go
@@ -0,0 +1,19 @@
+package main
+
+/*
+- Já entendemos ponteiros, já entendemos métodos. Já temos o conhecimento necessário para começar a utilizar a standard
+library.
+- Nesse vídeo faremos uma orientação sobre como abordar a documentação.
+- Essa aula não foi preparada. Vai ser tudo ao vivo no improviso pra vocês verem como funciona o processo.
+    - golang.org → Documents → Package Documentation
+    - godoc.org → encoding/json
+        - files
+        - examples
+        - funcs
+        - types
+        - methods
+*/
+
+func main() {
+
+}
