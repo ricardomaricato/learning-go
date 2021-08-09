4059a9912b7928ea6c0a1d0bedfafb761f21083f
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-09 12:19:39 -0300

    -Concorrência – 4. Na prática: Condição de corrida

---
+++ Cap.18_concorrencia/04_na-pratica_condicao-de-corrida/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+/*
+- Aqui vamos replicar a race condition mencionada no artigo anterior.
+    - time.Sleep(time.Second) vs. runtime.Gosched()
+- go help → go help build → go run -race main.go
+- Como resolver? Mutex.
+*/
+
+func main() {
+
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+
+	contador := 0
+	totaldegoroutines := 1000
+
+	var wg sync.WaitGroup
+	wg.Add(totaldegoroutines)
+
+	for i := 0; i < totaldegoroutines; i++ {
+		go func() {
+			v := contador
+			runtime.Gosched()
+			v++
+			contador = v
+			wg.Done()
+		}()
+		fmt.Println("Goroutines:", runtime.NumGoroutine())
+	}
+
+	wg.Wait()
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	fmt.Println("Valor final:", contador)
+
+}
