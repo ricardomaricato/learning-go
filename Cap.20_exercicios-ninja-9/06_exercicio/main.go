eec9006990bcd95cf9c2afaa8a1f3a8d4d51f279
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-10 17:15:00 -0300

    -Exercícios: Nível #9 – 6

---
+++ Cap.20_exercicios-ninja-9/06_exercicio/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+/*
+- Crie um programa que demonstra seu OS e ARCH.
+- Rode-o com os seguintes comandos:
+    - go run
+    - go build
+    - go install
+*/
+
+func main() {
+	fmt.Println("Seu OS é:\t", runtime.GOOS)
+	fmt.Println("Seu ARCH é:\t", runtime.GOARCH)
+}
