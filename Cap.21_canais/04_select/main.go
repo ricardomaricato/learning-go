8a8562925f7be3eabb710fa76653cd382087b0d3
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-11 12:45:48 -0300

    -Canais – 4. Select

--- Cap.21_canais/04_select/main.go
+++ Cap.21_canais/04_select/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 - Select é como switch, só que pra canais, e não é sequencial.
 - "A select blocks until one of its cases can run, then it executes that case. It chooses one at random if multiple are
@@ -23,5 +25,30 @@ ready." — https://tour.golang.org/concurrency/5
 */
 
 func main() {
+
+	a := make(chan int)
+	b := make(chan int)
+	x := 500
+
+	go func(x int) {
+		for i := 0; i < x; i++ {
+			a <- i
+		}
+	}(x / 2)
+
+	go func(x int) {
+		for i := 0; i < x; i++ {
+			b <- i
+		}
+	}(x / 2)
+
+	for i := 0; i < x; i++ {
+		select {
+		case v := <-a:
+			fmt.Println("Canal A:", v)
+		case v := <-b:
+			fmt.Println("Canal B:", v)
+		}
+	}
 
 }
