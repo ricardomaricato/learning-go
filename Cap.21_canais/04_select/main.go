e5d2bfa62118f2fa0968f60f987dd2f7c6d11ec8
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-11 12:35:18 -0300

    -Canais – 4. Select

---
+++ Cap.21_canais/04_select/main.go
@@ -0,0 +1,27 @@
+package main
+
+/*
+- Select é como switch, só que pra canais, e não é sequencial.
+- "A select blocks until one of its cases can run, then it executes that case. It chooses one at random if multiple are
+ready." — https://tour.golang.org/concurrency/5
+- Na prática:
+    - Exemplo 1:
+        - Duas go funcs enviando X/2 numeros cada uma pra um canal
+        - For loop X valores, select case ←x
+    - Exemplo 2:
+        - Func 1 recebe X valores de canal, depois manda qualquer coisa pra chan quit
+        - Func 2 for infinito, select: case envia pra canal, case recebe de quit
+    - Exemplo 3:
+        - Chans par, ímpar, quit
+        - Func send manda números pares pra um canal, ímpares pra outro, e fecha/quit
+        - Func receive é um select entre os três canais, encerra no quit
+        - Problema!
+- Go Playground:
+    - 1. https://play.golang.org/p/xC3e1wBxgv
+    - 2. https://play.golang.org/p/_NZqhBXN-v
+    - 3. https://play.golang.org/p/rK8QwsBo0H
+*/
+
+func main() {
+
+}
