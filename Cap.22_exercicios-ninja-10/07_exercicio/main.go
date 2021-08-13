f7085d93162fa7d2a547a1f9f1cd294265732c26
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-12 22:28:25 -0300

    -Exercícios: Nível #10 – 7

---
+++ Cap.22_exercicios-ninja-10/07_exercicio/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+/*
+- Crie um programa que lance 10 goroutines onde cada uma envia 10 números a um canal;
+- Tire estes números do canal e demonstre-os.
+*/
+
+func main() {
+	canal := make(chan int)
+	for i := 0; i < 10; i++ {
+		go func() {
+			for j := 0; j < 10; j++ {
+				canal <- j
+			}
+		}()
+	}
+	for k := 0; k < 100; k++ {
+		fmt.Println(k, "\t", <-canal)
+	}
+}
